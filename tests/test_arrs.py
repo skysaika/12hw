2dd7b6ef67a8b9898943d2d6dbf0e4ecd3479a9c
Saysuu <skysaika@yandex.ru>
2023-03-02 12:16:11 +0300

    assert in test/issue2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,9 +3,15 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
-    assert arrs.get([], -1, "test") == "test"
+    assert arrs.get([1, 2, 3, 4], 3, "test") == 4
+    assert arrs.get([], -2, "test") == "test"
+    assert arrs.get([1, 2], 1, "test") == 2
+    assert arrs.get([], -5, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
+    assert arrs.my_slice([], -1) == []
